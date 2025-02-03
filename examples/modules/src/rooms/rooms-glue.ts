c6e28ca21271e79eeebe9882ef1c5bb63d18fa1f
Alexandre VICTOOR <alexandrevictoor@mbp-de-admin-2.lan>
2025-02-03 09:20:59 +0100

    doc: commented out code in module example

--- examples/modules/src/rooms/rooms-glue.ts
+++ examples/modules/src/rooms/rooms-glue.ts
@@ -1,11 +1,15 @@
-// export const roomsGlue = Glue.buildFrom({
-//   roomsApiConfiguration: is<ApiConfiguration>,
-//   roomsReferentialClient: is<RoomsReferentialClient>,
-// });
+import { Glue, is } from "ts-glue";
+import { ApiConfiguration, buildRoomsReferentialClient } from "./api-client/rooms-referential-client";
+import { RoomsReferentialClient } from "./rooms";
 
-// export const setupRooms = roomsGlue.prepareSetup((glue) => {
-//   return glue.registerService(
-//     "roomsReferentialClient",
-//     glue.build(buildRoomsReferentialClient, ["roomsApiConfiguration"]),
-//   );
-// });
+export const roomsGlue = Glue.buildFrom({
+  roomsApiConfiguration: is<ApiConfiguration>,
+  roomsReferentialClient: is<RoomsReferentialClient>,
+});
+
+export const setupRooms = roomsGlue.prepareSetup((glue) => {
+  return glue.registerService(
+    "roomsReferentialClient",
+    glue.build(buildRoomsReferentialClient, ["roomsApiConfiguration"]),
+  );
+});
